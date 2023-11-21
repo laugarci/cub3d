e1774cb0a93bd9d48036ea7057aba5d1d4cd4bff
Laura Garcia Arch <garch.laura@gmail.com>
2023-11-21 11:28:46 +0100

    struct

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:02:22 by laugarci          #+#    #+#             */
-/*   Updated: 2023/11/21 10:03:26 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:28:35 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,18 @@
 #include <unistd.h>
 #include <stdio.h>
 
+typedef t_cub
+{
+	//PATH TO IMG
+	char *n;
+	char *s;
+	char *e;
+	char *w;
+	//COLORS
+	int *f;
+	int *c;
+	//MAP
+	char **map;
+}
 
 # endif

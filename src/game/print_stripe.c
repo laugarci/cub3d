a4e056952761eb6d8e931e2931d4fc857f92b0e5
Julia Olle roset <julia.olle.roset@gmail.com>
2023-11-27 15:13:16 +0100

    textures

--- src/game/print_stripe.c
+++ src/game/print_stripe.c
@@ -6,7 +6,7 @@
 /*   By: julolle- <julolle-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 10:23:03 by julolle-          #+#    #+#             */
-/*   Updated: 2023/11/27 12:43:15 by julolle-         ###   ########.fr       */
+/*   Updated: 2023/11/27 14:35:45 by julolle-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	get_text_num(t_rnd *rnd)
 float    find_x_onmap(t_win *wind, t_rnd *rnd)
 {
 	float	x_map;
-	//int		x_text;
+	int		x_text;
 	
 	if (rnd->side == 0)
 		x_map = wind->player->posy + rnd->perpwalldist * rnd->raydiry;
@@ -38,12 +38,12 @@ float    find_x_onmap(t_win *wind, t_rnd *rnd)
 		x_map = wind->player->posx + rnd->perpwalldist * rnd->raydirx;
 
 	x_map = x_map - floor((x_map)); //es queda el decimal
-	//x_text = (int)(x_map * 64);
-	/*if (rnd->side == 0 && rnd->raydiry > 0 )
+	x_text = (int)(x_map * 64);
+	if (rnd->side == 0 && rnd->raydiry > 0 )
 		x_text = 64 - x_text - 1;
 	else if (rnd->side == 1 && rnd->raydiry < 0)
-		x_text = 64 - x_text - 1;*/
-	return (x_map);
+		x_text = 64 - x_text - 1;
+	return (x_text);
 }
 
 int	get_pix_text(t_img *img, int x, int y)
@@ -69,7 +69,14 @@ void print_stripe(t_win *wind, t_rnd *rnd, int x)
 	x_text = find_x_onmap(wind, rnd);
 	step = (float)64 / rnd->line_height;
 
-	if (rnd->line_height >= HEIGHT)
+	/*while (y < 64)
+	{
+		color = get_pix_text(&wind->texture[n_text], x, y);
+		my_mlx_pixel_put(wind, x, y, color);
+		y++;
+	
+	}*/
+	if(rnd->line_height >= HEIGHT)
 		y_text = step * (rnd->line_height - HEIGHT) / 2;
 	else
 		y_text = 0;

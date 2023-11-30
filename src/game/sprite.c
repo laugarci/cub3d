7dde4d36d40cdda465ab24e81ce77ab03c455126
Julia Olle roset <julia.olle.roset@gmail.com>
2023-11-30 19:04:04 +0100

    update tetures

--- src/game/sprite.c
+++ src/game/sprite.c
@@ -6,7 +6,7 @@
 /*   By: julolle- <julolle-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 13:45:51 by laugarci          #+#    #+#             */
-/*   Updated: 2023/11/30 15:35:30 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/11/30 19:02:44 by julolle-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	put_player(t_player *player, t_win *wind)
 {
-	int err;
+	int	err;
 
 	if (player->direction == 1)
 		mlx_put_image_to_window(wind->mlx, wind->mlx_win, player->file[0], \
 			(WIDTH / 2) - 330, (HEIGHT - 550));
 	else if (player->direction == 2)
-		err = mlx_put_image_to_window(wind->mlx, wind->mlx_win, player->file[1], \
-			(WIDTH / 2) - 330, (HEIGHT - 550));
+		err = mlx_put_image_to_window(wind->mlx, wind->mlx_win, \
+			player->file[1], (WIDTH / 2) - 330, (HEIGHT - 550));
 }

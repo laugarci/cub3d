3814f39546c10ce37dbbbc429aa348519fcc3c2c
Laura Garcia Arch <laugarci@car7s2.42barcelona.com>
2023-12-04 15:47:49 +0100

    parser

--- src/parser/copy_map.c
+++ src/parser/copy_map.c
@@ -6,12 +6,25 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 17:30:31 by laugarci          #+#    #+#             */
-/*   Updated: 2023/12/04 15:28:00 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/12/04 15:47:14 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "cub3d.h"
+
+void	copy_map_aux(int k, int i, t_cub *cub, char **matrix)
+{
+	if (k < cub->cols)
+	{
+		while (k < (cub->cols - 1))
+		{
+			matrix[i][k] = '1';
+			k++;
+		}
+	}
+	matrix[i][k] = '\0';
+}
 
 void	copy_map(int init, t_cub *cub)
 {
@@ -28,7 +41,7 @@ void	copy_map(int init, t_cub *cub)
 		k = 0;
 		if (cub->all[init][0] == '\n' || cub->map[i] == NULL)
 			break ;
-		while (cub->all[init][k] && cub->all[init][k] != '\n')
+		while (k < (int)ft_strlen(cub->all[init]))
 		{
 			if (cub->all[init][k] == ' ')
 				cub->map[i][k] = '1';
@@ -36,6 +49,7 @@ void	copy_map(int init, t_cub *cub)
 				cub->map[i][k] = cub->all[init][k];
 			k++;
 		}
+		copy_map_aux(k, i, cub, cub->map);
 		i++;
 		init++;
 	}
